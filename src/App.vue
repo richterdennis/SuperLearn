49b6b2e1c1c53c0357314b7dc314023e946d39b6
Dennis Richter <dennis.richter@iem.thm.de>
2018-02-07 17:30:24 +0100

    Setup Project

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="app">
+        <div class="splashscreen"></div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+    name: 'app'
+});
+</script>
+
+<style>
+#app {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+
+.splashscreen {
+    background: url('./assets/logo_font.png') no-repeat center;
+    background-size: contain;
+    
+    position: absolute;
+    top: 10%;
+    left: 10%;
+    width: 80%;
+    height: 80%;
+}
+</style>
