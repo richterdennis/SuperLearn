10809cd50d2db3159762ebb228927eb3bacab101
Dennis Richter <dennis.richter@iem.thm.de>
2018-02-14 18:19:32 +0100

    Little changes on Navbar

--- src/components/md/Navbar.vue
+++ src/components/md/Navbar.vue
@@ -1,22 +1,25 @@
 <template>
-    <div>
-        <nav>
-            <div class="nav-wrapper">
-                <a href="#!" class="brand-logo"></a>
-                <a href="#" data-activates="mobile-nav" class="button-collapse"><i class="material-icons">menu</i></a>
-                <ul class="right hide-on-med-and-down">
-                    <li v-for="item in navitems" :key="item.name" :class="{'active': item.isActive}">
-                        <a href="#" @click="navItemClicked(item)">{{item.name}}</a>
-                    </li>
-                </ul>
-                <ul class="side-nav" id="mobile-nav">
-                    <li v-for="item in navitems" :key="item.name" :class="{'active': item.isActive}">
-                        <a href="#" @click="navItemClicked(item)">{{item.name}}</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-        <div class="container">
+    <div class="row">
+        <div class="navbar-fixed">
+            <nav>
+                <div class="nav-wrapper">
+                    <a href="#" class="brand-logo hide-on-med-and-down"></a>
+                    <span class="nav-title hide-on-large-only">{{currentItem.name}}</span>
+                    <a href="#" data-activates="mobile-nav" class="button-collapse"><i class="material-icons">menu</i></a>
+                    <ul class="right hide-on-med-and-down">
+                        <li v-for="item in navitems" :key="item.name" :class="{'active': item.isActive}">
+                            <a href="#" @click="navItemClicked(item)">{{item.name}}</a>
+                        </li>
+                    </ul>
+                    <ul class="side-nav" id="mobile-nav">
+                        <li v-for="item in navitems" :key="item.name" :class="{'active': item.isActive}">
+                            <a href="#" @click="navItemClicked(item)">{{item.name}}</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <div class="col s12 push-l1 l10">
             <slot></slot>
         </div>
     </div>
@@ -27,23 +30,56 @@ export default {
     name: 'navbar',
     data() {
         return {
-            navitems: []
+            navitems: [],
+            currentItem: {},
         }
     },
     created() {
         this.navitems = this.$children
+        this.navitems.some(item => {
+            if(item.isActive) {
+                this.currentItem = item;
+                return true;
+            }
+
+            return false;
+        });
     },
     mounted() {
         $(".button-collapse").sideNav();
     },
     methods: {
         navItemClicked(selectedItem) {
+            this.currentItem = selectedItem;
             this.navitems.forEach(item => {
                 item.isActive = (item.name == selectedItem.name);
             });
-
-            console.log(selectedItem);
         }
     }
 }
 </script>
+
+<style>
+.nav-title {
+    padding: 0 10px !important;
+}
+
+@media only screen and (max-width: 992px) {
+    .row .col {
+        padding-left: 0;
+        padding-right: 0;
+    }
+}
+
+@media only screen and (min-width: 993px) {
+    .row .col {
+        padding-top: 2rem;
+        padding-bottom: 2rem;
+    }
+
+    .row .col > div {
+        -webkit-box-shadow: 0 4px 5px 0 rgba(0,0,0,0.14), 0 1px 10px 0 rgba(0,0,0,0.12), 0 2px 4px -1px rgba(0,0,0,0.3);
+        box-shadow: 0 4px 5px 0 rgba(0,0,0,0.14), 0 1px 10px 0 rgba(0,0,0,0.12), 0 2px 4px -1px rgba(0,0,0,0.3);
+    }
+}
+</style>
