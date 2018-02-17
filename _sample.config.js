ae181f0943b196b57208ffcc16b50797b01acfd4
Dennis Richter <dennis.richter@iem.thm.de>
2018-02-17 15:57:34 +0100

    Add config and some global const

---
+++ _sample.config.js
@@ -0,0 +1,8 @@
+// Rename this file to config.js
+(function() {
+    window.App = window.App || {};
+    Object.assign(window.App, {
+        API_ADDRESS: "TYPE_IN_HERE_YOUR_API_ADDRESS",
+        API_APP_TOKEN: "TYPE_IN_HERE_YOUR_API_APP_TOKEN"
+    });
+})();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import Vue from "vue";
 import App from "./App.vue";
 
+window.App = window.App || {};
+Object.assign(window.App, {
+    VERSION: "v1.0.0-alpha.0",
+
+    CACHE: {
+        UUID: "UUID",
+        TOKEN: "TOKEN",
+        MODULES: "MODULES",
+        SEMESTER: "SEMESTER",
+        STUDIES_COURSES: "STUDIES_COURSES",
+        MY_QUESTIONS: "MY_QUESTIONS",
+        REPORTS: "REPORTS",
+        USER_ME: "USER_ME",
+        WALL_OF_FAME: "WALL_OF_FAME"
+    }
+});
+
 new Vue({
     el: "#app",
     render: h => h(App)
