092f62d7bf7031e5e2ed5972f2c90a9e8203baf1
Oguz <oguz1612@googlemail.com>
2018-02-26 18:15:45 +0100

    add images in RulesView

--- src/views/RulesView.vue
+++ src/views/RulesView.vue
@@ -2,6 +2,7 @@
 	<div id="rules-view">
 		<ul id="rule-list" class="collection">
   			<li v-for="rule in rules" :key="rule.title" class="collection-item">
+				  <img :src="rule.image" id="image"/>
 				  {{ rule.title }}<br>
 				  {{ rule.text }}
 				  </li>
@@ -16,9 +17,12 @@ export default{
 	data() {
 		return{ 
 			rules: [
-      		{title: '1. Regel' , text: 'Keine Anstößigen oder beleidigenden Usernamen wählen'},
-      		{title: '2. Regel' , text: 'Vor dem Hochladen muss die Korrektheit einer Frage geprüft werden'},
-			{title: '3. Regel' , text: 'Anstößige, falsche oder troll Fragen müssen gemeldet werden'},
+			  {title: '1. Regel' , text: 'Keine Anstößigen oder beleidigenden Usernamen wählen', 
+			  image: '../assets/rules_1.png'},
+      		{title: '2. Regel' , text: 'Vor dem Hochladen muss die Korrektheit einer Frage geprüft werden', 
+			  image: '../assets/rules_2.png'},
+			{title: '3. Regel' , text: 'Anstößige, falsche oder troll Fragen müssen gemeldet werden', 
+			  image: '../assets/rules_3.png'},
     		]
 		}
   	}
@@ -27,5 +31,10 @@ export default{
 </script>
 
 <style>
-	
+	#image{
+		float: left;
+		height: 45px;
+		width: auto;
+		margin-right: 2%;
+	}
 </style>
