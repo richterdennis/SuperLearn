d8cafded7acda86d914348e0f9926b6aae1a5d38
Dennis Richter <dennis.richter@iem.thm.de>
2018-02-26 11:55:01 +0100

    Use activity in main view

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,5 +1,5 @@
-<template>
-    <navbar id="main-view">
+<template><div>
+    <activity id="main-view" :title="title" icon="menu" layer="5">
         <navitem name="Home" :selected="true">
             <module-list-view></module-list-view>
         </navitem>
@@ -21,24 +21,65 @@
         </navitem>
         
         <navitem name="Logout" @click="doLogout"></navitem>
-    </navbar>
-</template>
+    </activity>
+
+    <ul class="side-nav" id="mobile-nav">
+        <li v-for="item in navitems" :key="item.name" :class="{'active': item.isActive}">
+            <a href="#" @click="navItemClicked(item)">{{item.name}}</a>
+        </li>
+    </ul>
+</div></template>
 
 <script>
-import Navbar from '../components/Navbar.vue';
+import Activity from '../components/Activity.vue';
 import Navitem from '../components/Navitem.vue';
 
 import ModuleListView from './ModuleListView.vue';
 
 export default {
     name: 'mainView',
+    data() {
+        return {
+            title: 'Home',
+            navitems: [],
+            currentItem: {},
+        }
+    },
     components: {
-        Navbar,
+        Activity,
         Navitem,
         ModuleListView
     },
+    mounted() {
+        this.navitems = this.$children[0].$children;
+        this.navitems.some(item => {
+            if(item.isActive) {
+                this.currentItem = item;
+                return true;
+            }
+
+            return false;
+        });
+
+        const $collapseButton = $("#main-view .button-collapse");
+        $collapseButton.attr('data-activates', 'mobile-nav');
+        $collapseButton.sideNav({
+            closeOnClick: true
+        });
+    },
     methods: {
+        navItemClicked(selectedItem) {
+            this.currentItem = selectedItem;
+            this.navitems.forEach(item => {
+                item.isActive = (item.name == selectedItem.name);
+            });
+
+            this.title = selectedItem.name;
+            selectedItem.$emit('click');
+        },
         doLogout() {
+            Cache.clear();
+            Cache.save();
             this.$emit('loggedInStateChange');
         }
     }
@@ -46,10 +87,4 @@ export default {
 </script>
 
 <style>
-a.brand-logo {
-    background: url('/assets/logo_font.png') no-repeat center;
-    background-size: contain;
-    height: 100%;
-    width: 200px;
-}
 </style>
