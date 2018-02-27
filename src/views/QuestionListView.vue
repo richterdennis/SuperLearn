e35222fca874bffb41d3c3c74e8628070ca98e07
Tobias Schaub <schaubi93@gmail.com>
2018-02-27 12:59:41 +0100

    Display answers in collapsible body

--- src/views/QuestionListView.vue
+++ src/views/QuestionListView.vue
@@ -2,7 +2,12 @@
     <ul id="question-list-view" class="collapsible" data-collapsible="accordion">
         <li v-for="question in questions" :key="question.id">
             <div class="collapsible-header">{{question.text}}<span class="badge">{{question.score}}</span></div>
-            <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+            <div class="collapsible-body">
+                <div class="row">
+                    <div class="col s6" v-if="question.questionType !== 1" v-for="answer in question.answers" :key="answer.id"><p>{{answer.text}}</p></div>
+                    <div class="col s12" v-else><span>{{ questionBooleanAnswer(question) }}</span></div>
+                </div>
+            </div>
         </li>
     </ul>
 </template>
@@ -23,6 +28,11 @@ export default {
         QuestionRouter.getQuestions().then(questions => {
             this.questions = questions;
         })
+    },
+    methods: {
+        questionBooleanAnswer(question) {
+            return question.answers[0].correct == 1? "Die Aussage ist korrekt!" : "Die Aussage ist falsch!";
+        }
     }
 }
 </script>
