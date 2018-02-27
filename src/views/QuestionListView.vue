a65a6b0e8a49a84af38ab35854a0ec1502cc1919
Tobias Schaub <schaubi93@gmail.com>
2018-02-27 13:34:15 +0100

    Added styling to answer display

--- src/views/QuestionListView.vue
+++ src/views/QuestionListView.vue
@@ -1,11 +1,11 @@
 <template>
     <ul id="question-list-view" class="collapsible" data-collapsible="accordion">
         <li v-for="question in questions" :key="question.id">
-            <div class="collapsible-header">{{question.text}}<span class="badge">{{question.score}}</span></div>
+            <div class="collapsible-header"><span class="truncate">{{question.text}}</span><span class="badge">{{question.score}}</span></div>
             <div class="collapsible-body">
                 <div class="row">
-                    <div class="col s6" v-if="question.questionType !== 1" v-for="answer in question.answers" :key="answer.id"><p>{{answer.text}}</p></div>
-                    <div class="col s12" v-else><span>{{ questionBooleanAnswer(question) }}</span></div>
+                    <div class="col s6" v-if="question.questionType !== 1" v-for="answer in question.answers" :key="answer.id"><div class="z-depth-1 answer incorrect">{{answer.text}}</div></div>
+                    <div class="col s12" v-else><div class="z-depth-1 answer correct">{{ questionBooleanAnswer(question) }}</div></div>
                 </div>
             </div>
         </li>
@@ -36,3 +36,20 @@ export default {
     }
 }
 </script>
+<style>
+    .answer {
+        border-style: solid;
+        border-width: 0 0 0 6px;
+        border-radius: 2px;
+        padding: 4px;
+        padding-left: 8px;
+        margin: 2px 0 6px 0;
+    }
+    .correct {
+        border-color: green;
+    }
+    .incorrect {
+        border-color: red;
+    }
+</style>
+
