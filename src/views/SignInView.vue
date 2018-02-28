8458a3d707ed258b2eb64a3005e4070413f39112
Julian <julianmies@web.de>
2018-02-28 10:25:07 +0100

    I should push more carefully
    fix

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -1,28 +1,30 @@
 <template>
-    <div class="section container">
-        <div class="row">
-            <div class="col s12 m6 offset-m3">
-                <div class="card logIn-wrapper">
-                    <div class="card-content">
-                        <form id="sign-in-view" @submit.prevent="doLogin">
-                            <div class="row">
-                                <div class="col s12 m6 offset-m3">
-                                    <img class="responsive-img" src="/../assets/logo_icon_font.png" alt="SuperLearn Logo">
+    <div id="sign-in-view">
+        <div class="section container">
+            <div class="row">
+                <div class="col s12 m6 offset-m3">
+                    <div class="card logIn-wrapper">
+                        <div class="card-content">
+                            <form id="sign-in-form" @submit.prevent="doLogin">
+                                <div class="row">
+                                    <div class="col s12 m6 offset-m3">
+                                        <img class="responsive-img" src="/../assets/logo_icon_font.png" alt="SuperLearn Logo">
+                                    </div>
                                 </div>
-                            </div>
-                            <h4 class="center green-text">Anmelden</h4>
-                            <div class="input-field">
-                                <label for="si-email" class="active">Email</label>
-                                <input id="si-email" name="si-email" type="email" class="validate" required v-model="email">
-                            </div>
-                            <div class="input-field">
-                                <label for="si-password">Passwort</label>
-                                <input id="si-password" name="si-password" type="password" class="validate" required v-model="password">
-                            </div>
-                            <input type="submit" class="btn-large green">      
-                            <a href="#">Passwort vergessen?</a><br>
-                            <a href="#">Noch kein Mitglied?</a>
-                        </form>
+                                <h4 class="center green-text">Anmelden</h4>
+                                <div class="input-field">
+                                    <label for="si-email" class="active">Email</label>
+                                    <input id="si-email" name="si-email" type="email" class="validate" required v-model="email">
+                                </div>
+                                <div class="input-field">
+                                    <label for="si-password">Passwort</label>
+                                    <input id="si-password" name="si-password" type="password" class="validate" required v-model="password">
+                                </div>
+                                <input type="submit" class="btn-large green">      
+                                <a href="#">Passwort vergessen?</a><br>
+                                <a href="#">Noch kein Mitglied?</a>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -64,16 +66,15 @@ export default {
         }
     }
 }
-/* var pattern = Trianglify({
-    height: 300,
-    width: 300,
-    cell_size: 40});
-
-    document.body.appendChild(pattern.canvas()); */
 </script>
 
 <style>
-#app {
+#sign-in-view {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
     background-color: #f4f4f4;
 }
 input[type=submit] {
--- src/views/SignOnView.vue
+++ src/views/SignOnView.vue
@@ -1,47 +1,49 @@
 <template>
-    <div class="section container">
-        <div class="row">
-            <div class="col s12 m6 offset-m3">
-                <div class="card logOn-wrapper">
-                    <div class="card-content">
-                        <form id="sign-on-view" @submit.prevent="doRegister">
-                            <div class="row">
-                                <div class="col s12 m6 offset-m3">
-                                    <img class="responsive-img" src="/../assets/logo_icon_font.png" alt="SuperLearn Logo">
+    <div id="sign-on-view">
+        <div class="section container">
+            <div class="row">
+                <div class="col s12 m6 offset-m3">
+                    <div class="card logOn-wrapper">
+                        <div class="card-content">
+                            <form id="sign-on-form" @submit.prevent="doRegister">
+                                <div class="row">
+                                    <div class="col s12 m6 offset-m3">
+                                        <img class="responsive-img" src="/../assets/logo_icon_font.png" alt="SuperLearn Logo">
+                                    </div>
                                 </div>
-                            </div>
-                            <h4 class="center green-text">Account erstellen</h4>
+                                <h4 class="center green-text">Account erstellen</h4>
 
-                            <div class="input-field">
-                                <input id="so-email" name="so-email" type="email" class="validate" required v-model="email">
-                                <label for="so-email">Email</label>
-                            </div>
-                            <div class="input-field">
-                                <input id="so-nickname" name="so-nickname" type="text" class="validate" required v-model="nickname">
-                                <label for="so-nickname">Nickname</label>
-                            </div>
-                            <div class="input-field">
-                                <input id="so-password" name="so-password" type="password" class="validate" required v-model="password">
-                                <label for="so-password">Passwort</label>
-                            </div>
-                            <div class="input-field">
-                                <input id="so-password2" name="so-password2" type="password" class="validate" required v-model="password2">
-                                <label for="so-password2">Passwort wiederholen</label>
-                            </div>
-                            <div class="input-field">
-                                <select id="so-courses" name="so-courses" class="validate" required v-model="course" ref="so-courses">
-                                    <option disabled value="-1">-- bitte wählen --</option>
-                                    <option v-for="course in courses" :key="course.id" :value="course.id">{{course.text}}</option>
-                                </select>
-                                <label for="so-courses">Studiengang</label>
-                            </div>
-                                <p>
-                                    <input type="checkbox" id="rules_ckeck" v-model="checked" />
-                                    <label for="rules_ckeck">Bitte lese unsere <a href="#">Regeln</a> bevor du einen Account erstellst.</label>
-                                </p>
-                            <input type="submit" class="btn-large green" :disabled="!checked">      
-                            <a href="#">Du hast schon einen Account?</a><br>
-                        </form>
+                                <div class="input-field">
+                                    <input id="so-email" name="so-email" type="email" class="validate" required v-model="email">
+                                    <label for="so-email">Email</label>
+                                </div>
+                                <div class="input-field">
+                                    <input id="so-nickname" name="so-nickname" type="text" class="validate" required v-model="nickname">
+                                    <label for="so-nickname">Nickname</label>
+                                </div>
+                                <div class="input-field">
+                                    <input id="so-password" name="so-password" type="password" class="validate" required v-model="password">
+                                    <label for="so-password">Passwort</label>
+                                </div>
+                                <div class="input-field">
+                                    <input id="so-password2" name="so-password2" type="password" class="validate" required v-model="password2">
+                                    <label for="so-password2">Passwort wiederholen</label>
+                                </div>
+                                <div class="input-field">
+                                    <select id="so-courses" name="so-courses" class="validate" required v-model="course" ref="so-courses">
+                                        <option disabled value="-1">-- bitte wählen --</option>
+                                        <option v-for="course in courses" :key="course.id" :value="course.id">{{course.text}}</option>
+                                    </select>
+                                    <label for="so-courses">Studiengang</label>
+                                </div>
+                                    <p>
+                                        <input type="checkbox" id="rules_check" v-model="checked" />
+                                        <label for="rules_check">Bitte lese unsere <a href="#">Regeln</a> bevor du einen Account erstellst.</label>
+                                    </p>
+                                <input type="submit" class="btn-large green" :disabled="!checked">      
+                                <a href="#">Du hast schon einen Account?</a><br>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -74,6 +76,8 @@ export default {
         }).catch(err => {
             throw 'Something went wrong!';
         });
+
+       addTriangleTo(document.getElementById('sign-on-view'));
     },
     methods: {
         doRegister() {
@@ -90,13 +94,26 @@ export default {
             }).catch(e => {
                 throw 'Something went wrong!';
             });
+        },
+        addTriangleTo(target) {
+            var dimensions = target.getClientRects()[0];
+            var pattern = Trianglify({
+            width: dimensions.width, 
+            height: dimensions.height
+        });
+        target.style['background-image'] = 'url(' + pattern.png() + ')';
         }
     }
 }
 </script>
 
 <style>
-#app {
+#sign-on-view {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
     background-color: #f4f4f4;
 }
 input[type=submit] {
