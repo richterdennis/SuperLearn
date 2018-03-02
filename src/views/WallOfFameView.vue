f23ad5918b04b1d1a7def516a297dc2c5d524432
Oguz <oguz1612@googlemail.com>
2018-03-02 19:43:01 +0100

    add style and userscore

--- src/views/WallOfFameView.vue
+++ src/views/WallOfFameView.vue
@@ -2,8 +2,13 @@
     <div id="wall-of-fame-view">
         <ul id="wall-of-fame-list" class="collection">
   			<li v-for="(user, index) in users" :key="user.id" class="collection-item">
-				  {{ index + 1 }}. {{ user.nickname }}
-				  </li>
+				<span class="badge white-text gold" v-if="index == 0">{{ index + 1 }}.</span>
+                <span class="badge white-text silber" v-if="index == 1">{{ index + 1 }}.</span>
+                <span class="badge white-text bronze" v-if="index == 2">{{ index + 1 }}.</span>
+                <span class="badge white-text superLearn green" v-if="index > 2">{{ index + 1 }}.</span>
+                {{ user.nickname }}
+                <div class="userscore">score: {{ user.score }}</div>
+			</li>
 		</ul>
     </div>
 </template>
@@ -26,3 +31,25 @@ export default{
     }
 }
 </script>
+
+<style>
+    .superLearn.green{
+        background-color: #00C853 !important;
+    }
+    .gold{
+        background-color: #D4AF37;
+    }
+    .silber{
+        background-color: #C0C0C0;
+    }
+    .bronze{
+        background-color: #CD7F32;
+    }
+    div.userscore{
+        float: right;
+    }
+    span.badge{
+        margin-right: 2%;
+        float: left;
+    }
+</style>
