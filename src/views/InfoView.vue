d6f7a960fb69087f3e48886ffd0b6004a5bc892c
Oguz <oguz1612@googlemail.com>
2018-03-04 18:02:10 +0100

    add style

--- src/views/InfoView.vue
+++ src/views/InfoView.vue
@@ -1,23 +1,21 @@
 <template>
 	<div id="info-view">
-        <h4>SuperLearn</h4>
-        <h6>Version: 1.0.0</h6>
+        <h4 class="title">SuperLearn</h4>
+        <h6 class="version">Version: 1.0.0</h6>
 		<p>SuperLearn entsteht im Rahmen des Moduls "MT70 Webanwendungen (Webprogrammierung 2)" 
         der THM. Ziel ist es eine Lernplattform für Studierende zu schaffen. 
         Vorerst wird nur die THM und der Studiengang Medieninformatik unterstützt, 
         weitere folgen erst nach dem Release.</p>
-        <h5>Entwickler</h5>
-        <p v-for="dev in developer" :key="dev.name">
+        <h5 class="title_dev">Entwickler:</h5>
+        <div class="card-panel text-grey" v-for="dev in developer" :key="dev.name">
+            <img class="bild circle" :src="url"/>
             {{dev.name}}<br>
-            {{dev.job}}
-            </p>
+            <div class="job">{{dev.job}}</div>
+        </div>
 	</div>
-
 </template>
 
 <script>
-import {version} from '../../package.json'
-
 export default{
 	name: 'dev-list',
 	data() {
@@ -28,12 +26,35 @@ export default{
             {name: 'Tobias Schaub' , job: 'Entwickler'},
             {name: 'Oguz Bulunmaz' , job: 'Entwickler'},
             ],
-		}
+
+            url: "../assets/profile_picture.png",
+        }
   	}
 }
-
 </script>
 
 <style>
-
+    #info-view{
+        padding-left: 3%;
+        padding-right: 3%;
+        padding-bottom: 2%;
+    }
+    .title{
+        padding-top: 2%;
+    }
+    .version{
+        color: darkgrey;
+        font-style: italic;
+    }
+    .bild{
+        position: relative;
+        float: left;
+        height: 60px;
+        width: 60px;
+        top: -9px;
+        left: -9px;
+    }
+    .job{
+        font-size: 12px;
+    }
 </style>
