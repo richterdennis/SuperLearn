5aa78dcdc6a76c874eca2bf070aa7d593ba37abd
Julian <julianmies@web.de>
2018-03-05 23:08:09 +0100

    Round Response styles

--- src/views/QuestionListItem.vue
+++ src/views/QuestionListItem.vue
@@ -1,9 +1,9 @@
 <template>
     <li class="question-list-item">
-        <div class="collapsible-header"><i class="material-icons" v-if="!answer.correct">highlight_off</i><i class="material-icons" v-else>check</i>{{question.text}}</div>
+        <div class="collapsible-header"><i class="material-icons red-text" v-if="!answer.correct">highlight_off</i><i class="material-icons" v-else v-bind:class="{'green-text accent-4':answer.correct}">check</i>{{question.text}}</div>
         <div class="collapsible-body">
-            <div class="question-text">Deine Antwort war {{answer.correct ? 'richtig' : 'falsch'}}!</div>
-            <div class="question-correct">Korrekte Antwort: {{answer.text}}!</div>
+            <div class="question-text"><blockquote v-bind:class="{'correct_color':answer.correct}">Deine Antwort war {{answer.correct ? 'richtig' : 'falsch'}}!</blockquote></div>
+            <div class="question-correct"><blockquote class="correct_color">Korrekte Antwort: <strong>{{answer.text}}</strong></blockquote></div>
          </div>
     </li>
 </template>
@@ -19,5 +19,15 @@ export default {
 </script>
 
 <style>
+.correct_color{
+  margin: 10px 0;
+  border-left: 5px solid #64dd17; 
+}
+blockquote {
+    margin: 10px 0;
+}
+div.collapsible-header:hover{
+    background-color: #ddd;
+}
 
 </style>
