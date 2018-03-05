2f69ef26d1e4b1681e200c77c4043d144857df4a
Tobias Schaub <schaubi93@gmail.com>
2018-03-05 16:15:58 +0100

    Added Floating Action Button

--- src/views/QuestionListView.vue
+++ src/views/QuestionListView.vue
@@ -1,52 +1,59 @@
 <template>
-    <ul id="question-list-view" class="collapsible" data-collapsible="accordion">
-        <li>
-            <div class="questionlist-header">
-                <div class="row nomargin">
-                    <div class="input-field col s12">
-                        <input type="text" name="searchString" id="searchString" v-model="searchString">
-                        <label for="searchString">Suchbegriff</label>
+    <div class="question-list-view">
+        <ul class="collapsible" data-collapsible="accordion">
+            <li>
+                <div class="questionlist-header">
+                    <div class="row nomargin">
+                        <div class="input-field col s12">
+                            <input type="text" name="searchString" id="searchString" v-model="searchString">
+                            <label for="searchString">Suchbegriff</label>
+                        </div>
+                    </div>
+                    <div class="row nomargin">
+                        <div class="input-field col s6">
+                            <select name="so-sortby" id="so-sortby" v-model="sortby" ref="so-sortby">
+                                <option value="-1" disabled selected>Sortieren nach...</option>
+                                <option v-for="sortby in sortbyOptions" :key="sortby.id" :value="sortby.id">{{ sortby.text }}</option>
+                            </select>
+                            <label for="so-sortby">Sortieren nach...</label>
+                        </div>
+                        <div class="input-field col s6">
+                            <select name="so-orderby" id="so-orderby" v-model="orderby" ref="so-orderby">
+                                <option value="-1" disabled selected>Sortierreihenfolge</option>
+                                <option v-for="orderby in orderbyOptions" :key="orderby.id" :value="orderby.id">{{ orderby.text }}</option>
+                            </select>
+                            <label for="so-sortby">Sortierreihenfolge</label>
+                        </div>
                     </div>
                 </div>
-                <div class="row nomargin">
-                    <div class="input-field col s6">
-                        <select name="so-sortby" id="so-sortby" v-model="sortby" ref="so-sortby">
-                            <option value="-1" disabled selected>Sortieren nach...</option>
-                            <option v-for="sortby in sortbyOptions" :key="sortby.id" :value="sortby.id">{{ sortby.text }}</option>
-                        </select>
-                        <label for="so-sortby">Sortieren nach...</label>
-                    </div>
-                    <div class="input-field col s6">
-                        <select name="so-orderby" id="so-orderby" v-model="orderby" ref="so-orderby">
-                            <option value="-1" disabled selected>Sortierreihenfolge</option>
-                            <option v-for="orderby in orderbyOptions" :key="orderby.id" :value="orderby.id">{{ orderby.text }}</option>
-                        </select>
-                        <label for="so-sortby">Sortierreihenfolge</label>
-                    </div>
-                </div>
-            </div>
-        </li>
-        <li v-for="question in filteredQuestions" :key="question.id">
-            <div class="collapsible-header"><span class="truncate">{{question.text}}</span><span class="badge">{{ question.score }}</span></div>
-            <div class="collapsible-body">
-                <div class="row nomargin">
-                    <div class="col s12" v-if="question.questionType == 1">
-                        <div :class="isCorrectAnswer(question.answers[0])">{{ questionBooleanAnswer(question) }}</div>
-                    </div>
-                    <div class="col s6" v-else-if="question.questionType == 2" v-for="answer in question.answers" :key="answer.id">
-                        <div :class="isCorrectAnswer(answer)">{{answer.text}}</div>
-                    </div>
-                    <div class="col s12" v-else-if="question.questionType == 3">
-                        <div :class="isCorrectAnswer(question.answers[0])">{{ question.answers[0].text }}</div>
-                    </div>
-                    <div class="col s6">
-                        <p class="smalltext">Erstellt am:</p>
-                        <p>{{ timeStampToDate(question.created*1000) }}</p>
+            </li>
+            <li v-for="question in filteredQuestions" :key="question.id">
+                <div class="collapsible-header"><span class="truncate">{{question.text}}</span><span class="badge">{{ question.score }}</span></div>
+                <div class="collapsible-body">
+                    <div class="row nomargin">
+                        <div class="col s12" v-if="question.questionType == 1">
+                            <div :class="isCorrectAnswer(question.answers[0])">{{ questionBooleanAnswer(question) }}</div>
+                        </div>
+                        <div class="col s6" v-else-if="question.questionType == 2" v-for="answer in question.answers" :key="answer.id">
+                            <div :class="isCorrectAnswer(answer)">{{answer.text}}</div>
+                        </div>
+                        <div class="col s12" v-else-if="question.questionType == 3">
+                            <div :class="isCorrectAnswer(question.answers[0])">{{ question.answers[0].text }}</div>
+                        </div>
+                        <div class="col s6">
+                            <p class="smalltext">Erstellt am:</p>
+                            <p>{{ timeStampToDate(question.created*1000) }}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </li>
-    </ul>
+            </li>
+        </ul>
+        <div class="fixed-action-btn">
+            <a class="btn-floating btn-large red waves-effect waves-light">
+                <i class="large material-icons">add</i>
+            </a>
+        </div>
+    </div>
 </template>
 
 <script>
