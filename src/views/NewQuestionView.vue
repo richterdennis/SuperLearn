cda126ea9deeffd68e6645fff95ad1fedf9e9038
Tobias Schaub <schaubi93@gmail.com>
2018-03-05 17:05:14 +0100

    Created basic layout

--- src/views/NewQuestionView.vue
+++ src/views/NewQuestionView.vue
@@ -1,7 +1,26 @@
 <template>
     <activity id="new-question-view" :title="title" icon="close" layer="10" @onIconClicked="close">
-        <div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis rem consectetur fugiat perferendis beatae magni error tenetur impedit accusamus saepe, nisi explicabo laboriosam voluptatem totam dolore corporis suscipit optio architecto!</p>
+        <div class="row">
+            <div class="input-field col s12">
+                <select name="select-course" id="select-course">
+                    <option value="-1" disabled selected>Bitte wähle einen Kurs...</option>
+                </select>
+                <label for="select-course">Kurs</label>
+            </div>
+            <div class="input-field col s12">
+                <select name="select-question-type" id="select-question-type">
+                    <option value="-1" disabled selected>...und einen Fragetypen</option>
+                </select>
+                <label for="select-question-type">Fragetyp</label>
+            </div>
+            <div class="input-field col s12">
+                <input type="text" name="input-questionText" id="input-questionText" required>
+                <label for="input-questionText">Fragetext</label>
+            </div>
+            <template>
+                <h1>Template hier einfügen...</h1>
+            </template>
+            <button type="submit" class="col s12 waves-effect waves-light btn">FRAGE ABSENDEN</button>
         </div>
     </activity>
 </template>
@@ -18,6 +37,9 @@ export default {
         close() {
             this.$emit('close');
         }
+    },
+    mounted() {
+        $('select').material_select();
     }
 }
 </script>
