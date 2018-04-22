02bc9fa9fd6c627136cc99bcb939f86db7b7a588
Tobias Schaub <schaubi93@gmail.com>
2018-04-22 14:47:22 +0200

    Created base for reports

--- src/views/QuestionReportView.vue
+++ src/views/QuestionReportView.vue
@@ -1,11 +1,14 @@
 <template>
     <div class="question-report-view modal">
         <div class="modal-content">
-            <h4>Modal Header</h4>
-            <p>A bunch of text</p>
+            <h4>Frage melden</h4>
+            <form class="row" id="question-report-form" @submit.prevent="submitReport">
+                <p>{{question}}</p>
+            </form>
         </div>
         <div class="modal-footer">
-            <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+            <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Abbrechen</a>
+            <a href="#!" class="modal-action waves-effect waves-green btn-flat">Senden</a>
         </div>
     </div>
 </template>
