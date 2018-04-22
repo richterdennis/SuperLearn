57c18ce9f2bda046ce29940247900342df4a4999
Tobias Schaub <schaubi93@gmail.com>
2018-04-22 18:53:24 +0200

    Created report-form

--- src/views/QuestionReportView.vue
+++ src/views/QuestionReportView.vue
@@ -1,9 +1,20 @@
 <template>
     <div class="question-report-view modal">
         <div class="modal-content">
-            <h4>Frage melden</h4>
+            <h4>Frage #{{ question.id }} melden</h4>
+            <p class="flow-text center ">{{ question.text }}</p>
             <form class="row" id="question-report-form" @submit.prevent="submitReport">
-                <p>{{question}}</p>
+                <div class="input-field col s12">
+                    <select name="select-reason" id="select-reason">
+                        <option value="-1" disabled selected>Bitte wählen...</option>
+                        <option v-for="reason in reportReasons" :value="reason.id" :key="reason.id">{{ reason.text }}</option>
+                    </select>
+                    <label for="select-reason">Was ist der Grund für die Meldung?</label>
+                </div>
+                <div class="input-field col s12">
+                    <textarea name="text-reason-specific" id="text-reason-specific" class="materialize-textarea"></textarea>
+                    <label for="text-reason-specific">Beschreibe den Grund genauer!</label>
+                </div>
             </form>
         </div>
         <div class="modal-footer">
@@ -15,6 +26,26 @@
 
 <script>
 export default {
-    props: ['question']
+    data: function() {
+        return {
+            reportReasons: [
+                { id: 1, text: "Anstößiger Inhalt" },
+                { id: 2, text: "Doppelt vorhanden" },
+                { id: 3, text: "Rechtschreibung" },
+                { id: 4, text: "Trollfrage" },
+                { id: 5, text: "Sonstiges" }                
+            ]
+        }
+    },
+    props: ['question'],
+    mounted() {
+        $('select').material_select();
+    }
 }
 </script>
+
+<style>
+    .question-report-view #question-report-form {
+        margin-top: 12px;
+    }
+</style>
