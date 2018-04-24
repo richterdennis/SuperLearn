d7689a1cd8adf3ef761ebc5fda5c7b8f9295568d
Dennis Richter <richter.dennis@live.de>
2018-04-24 20:26:35 +0200

    Load real user from user id

--- src/views/QuestionInfoView.vue
+++ src/views/QuestionInfoView.vue
@@ -4,11 +4,11 @@
             <h5>Infos zur Frage</h5>
             <hr/>
             <div style="font-size: calc(0.75em + 1vmin);">
-                id: <b>{{question.id}}</b><br>
-                Ersteller: <b style="color: blue">{{question.userId}}</b><br>
+                Id: <b>{{question.id}}</b><br>
+                Ersteller: <b>{{questionCreator.nickname || question.userId}}</b><br>
                 Erstellt am: <b>{{QuestionCreated(question.created *1000)}}</b><br>
                 Beantwortet: <b>{{question.answeredCounter}}</b><br>
-                davon Falsch: <b>{{question.wrongCounter}}</b>
+                Davon Falsch: <b>{{question.wrongCounter}}</b>
             </div>
             <hr/>
         </div>
@@ -19,10 +19,23 @@
 </template>
 
 <script>
+import { UserRouter } from "../classes/Router.js";
+
 export default {
     name: 'question-info-view',
     props: ['question'],
-
+    data() {
+        return {
+            questionCreator: {}
+        }
+    },
+    watch: {
+        question: function() {
+            UserRouter.getUser(this.question.userId).then(user => {
+                this.questionCreator = user
+            });
+        }
+    },
     methods: {
         QuestionCreated(timestamp) {
             return new Date(timestamp).toLocaleDateString();
